832ef25800d7732c020405654aa5c060d485b7ee
siddhant-agrawal01 <siddhantagrawal001@gmail.com>
2026-05-24 23:15:35 +0530

    feat(download):add yt-dlp for saving files

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -7,3 +7,25 @@ import os
 DOWNLOAD_DIRECTORY='downloades'
 
 os.makedirs(DOWNLOAD_DIRECTORY,exist_ok=true);
+
+
+def download_youtube_audio(url:str)->str:
+    output_path=os.path.join(DOWNLOAD_DIRECTORY,"%(title)s.%(ext)s")
+    ydl_options={
+        "format":"bestaudio/best",
+        "outtmpl":output_path,
+       "postprocessors":[
+        {
+            "key":"FFmpegExtractAudio",
+            "preferredcodec":"wav",
+            "preferredquality":"192",
+        }
+       ],
+       "quiet":True,
+    }
+    with yt_dlp.YoutubeDl(ydl_opts) as ydl:
+        info=ydl.extract_info(url,download=True)
+        filename=ydl.prepare_filename(info).replace(".webm",".wav").replace(".mp4",".wav")
+    return filename
+    # //converting file to wav using pydub
+    
