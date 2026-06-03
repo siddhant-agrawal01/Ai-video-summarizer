85824a869ee19a31b6cb5ed4c245c10186c0e3a6
siddhant-agrawal01 <siddhantagrawal001@gmail.com>
2026-06-03 22:22:03 +0530

    fix->Audio files are downloading and saving

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -6,7 +6,7 @@ import os
 # //saving audio to a directorty
 DOWNLOAD_DIRECTORY='downloades'
 
-os.makedirs(DOWNLOAD_DIRECTORY,exist_ok=true);
+os.makedirs(DOWNLOAD_DIRECTORY,exist_ok=True);
 
 
 def download_youtube_audio(url:str)->str:
@@ -23,9 +23,11 @@ def download_youtube_audio(url:str)->str:
        ],
        "quiet":True,
     }
-    with yt_dlp.YoutubeDl(ydl_opts) as ydl:
+    with yt_dlp.YoutubeDL(ydl_options) as ydl:
         info=ydl.extract_info(url,download=True)
         filename=ydl.prepare_filename(info).replace(".webm",".wav").replace(".mp4",".wav")
     return filename
-    # //converting file to wav using pydub
-    
+# //converting file to wav using pydub
+if __name__ == "__main__":
+    result = download_youtube_audio("https://www.youtube.com/shorts/0E2MogEpNx0")
+    print(f"Downloaded to: {result}")
